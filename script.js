fce8281f6c7db36d68aef1370c9f8e64dedde41d
Justin W <kidspeedy@gmail.com>
2022-08-11 22:14:24 -0700

    Almost done with background image APOD

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var dailyURL = "";
 fetch('https://api.nasa.gov/planetary/apod?api_key=UYBHJ04Q9lLfBjmou4hJQs8ZUPsR0OrRHdvJxaHC')
     .then(function (response) {
         return response.json();
@@ -5,5 +6,7 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=UYBHJ04Q9lLfBjmou4hJQs8ZUPsR0
     .then(function (data) {
         console.log(data);
         dailyURL = data.hdurl;
-        console.log(dailyURL);
+    })
+    .then(function () {
+        $("html").css("background", "url(" + dailyURL + ")").css("background-size", "cover");
     })
