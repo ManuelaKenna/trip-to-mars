96c73431c71c4a437f7888297bc398fc8b9be62b
Justin W <kidspeedy@gmail.com>
2022-08-16 21:07:01 -0700

    Testing local storage, moving on to work with Deborah's pages and local storage

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=UYBHJ04Q9lLfBjmou4hJQs8ZUPsR0
         $("html").css("background-image", "url(" + dailyURL + ")").css("height", "100%").css("background-position", "center").css("background-repeat", "no-repeat").css("background-size", "cover");
     })
 
+// Button to give extra info on background image
+$( "#btn4" ).click(function() {
+
+  });
+
+// Messing with local storage
+$( "#btn1" ).click(function() {
+    localStorage.setItem('myCat', 'Tom');
+  });
+$( "#btn2" ).click(function() {
+    localStorage.setItem('myCat', 'Bob');
+  });
+$( "#btn3" ).click(function() {
+    var myCatName = localStorage.getItem('myCat');
+    alert( myCatName);
+  });
