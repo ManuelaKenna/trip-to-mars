7f54581507baa7285e96eea5714970a21a41bff5
Justin W <kidspeedy@gmail.com>
2022-08-16 19:17:26 -0700

    Fixed sizing for background-image and added backup image

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var dailyURL = "";
+var backup = "backup.jpg";
+// Stock background
+$("html").css("background-image", "url(" + backup + ")").css("height", "100%").css("background-position", "center").css("background-repeat", "no-repeat").css("background-size", "cover");
+// Fetch APOD background
 fetch('https://api.nasa.gov/planetary/apod?api_key=UYBHJ04Q9lLfBjmou4hJQs8ZUPsR0OrRHdvJxaHC')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
         dailyURL = data.hdurl;
     })
     .then(function () {
-        $("html").css("background", "url(" + dailyURL + ")").css("background-size", "cover");
+        $("html").css("background-image", "url(" + dailyURL + ")").css("height", "100%").css("background-position", "center").css("background-repeat", "no-repeat").css("background-size", "cover");
     })
+
