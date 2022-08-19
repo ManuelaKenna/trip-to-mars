1105ad2e62d506b299194cf26b8aabd6279f4529
Justin W <kidspeedy@gmail.com>
2022-08-19 02:27:42 -0700

    added some css to body to make progress bar more complete

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// TODO: Fix Jquery css notations
+
 // Declaring/defining variables for background images
 var dailyURL = "";
 var backup = "assets/images/backup.jpg";
-// Stock background
-$("html").css("background-image", "url(" + backup + ")").css("height", "100%").css("background-position", "center").css("background-repeat", "no-repeat").css("background-size", "cover").css("object-fit","cover");
-// Fetch APOD background
+
+// Set backup stock background
+$("html").css({"background-image":"url(" + backup + ")","height":"100%",
+"object-fit":"cover","width":"100%","background-position":"center","background-repeat":"no-repeat","background-size": "cover"});
+
+// Fetch APOD background and set to page
 fetch('https://api.nasa.gov/planetary/apod?api_key=UYBHJ04Q9lLfBjmou4hJQs8ZUPsR0OrRHdvJxaHC')
     .then(function (response) {
         return response.json();
@@ -12,18 +17,28 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=UYBHJ04Q9lLfBjmou4hJQs8ZUPsR0
         dailyURL = data.hdurl;
     })
     .then(function () {
-        $("html").css("background-image", "url(" + dailyURL + ")").css("height", "100%").css("object-fit","cover").css("width","100%").css("background-position", "center").css("background-repeat", "no-repeat").css("background-size", "cover");
+        $("html").css({"background-image":"url(" + dailyURL + ")","height":"100%",
+        "object-fit":"cover","width":"100%","background-position":"center","background-repeat":"no-repeat","background-size": "cover"});
 })
+
+// Adding text to body
+var userName = "Justin";
+var goalString = "drink 5 glasses of water";
+
+$(`#goalTitle`).children([0]).prepend("Daily Goal of: " + userName);
+
+$(`#goalQuestion`).children([0]).append("Did you " + goalString + " today?");
+
 
 
 // Declaring/defining variables for progress bar
 var htmlString = "";
 var goalDays = 8; // Change to local storage from previous
-var dayCurrent = 2; // Change to local storage from previous
+var dayCurrent = 6; // Change to local storage from previous
 
 // Adding Earth at the beginning
 $(`#timeline`).append(`<img id="EarthImg" src="assets/images/Earth.jpg" />`);
-$('#timeline').children([1]).css("height","4rem").css("width","4rem").css("2px","solid grey").css("border-radius","50%");
+$(`#EarthImg`).css("height","4rem").css("width","4rem").css("2px","solid grey").css("border-radius","50%");
 
 // Creating for loop to make progress bar based on amount of goals days
 for (let c = 0; c < goalDays; c++) {
@@ -41,11 +56,11 @@ for (let c = 0; c < dayCurrent; c++) {
 
 // Adding Rover picture over current day
 $(`#box${dayCurrent}`).append(`<img id="roverImg" src="assets/images/rover.png" />`)
-$(`#box${dayCurrent}`).children().css("height","4rem").css("width","4rem").css("position","absolute").css("top","-5rem").css("left","0rem");
+$(`#box${dayCurrent}`).children().css("height","4rem").css("width","4rem").css("position","absolute").css("top","-5rem").css("left","-1.2rem");
 
 // Adding Mars at the end
 $(`#timeline`).append(`<img id="MarsImg" src="assets/images/Mars.jpg" />`);
-$('#timeline').children([1]).css("height","4rem").css("width","4rem").css("2px","solid grey").css("border-radius","50%");
+$(`#MarsImg`).css("height","4rem").css("width","4rem").css("2px","solid grey").css("border-radius","50%");
 
 
 
