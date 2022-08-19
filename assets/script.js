0c42dea89df5f70a31931d2e151d1a47005e8bbe
Manuelacreate <107735377+Manuelacreate@users.noreply.github.com>
2022-08-18 19:17:56 -0700

    finished form

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,24 @@ $(document).ready(function() {
     M.updateTextFields();
   });
         
-  
   $('#textarea1').val('New Text');
   M.textareaAutoResize($('#textarea1'));
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+    var instance = M.FormSelect.init(elems, options);
+    instance.getSelectedValues();
+  });
+  
+  $(document).ready(function(){
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown();
+ });
+
+  $(document).ready(function(){
+    $('select').formSelect();
   });
 
-  var instance = M.FormSelect.getInstance(elem);
+  var instance = M.FormSelect.getInstances(elem);
 
   instance.getSelectedValues();
