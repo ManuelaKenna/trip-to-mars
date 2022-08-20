15b0311d35b888d4051195296be9fdfbea459e7b
Lynchk07 <lynch.kristinj@gmail.com>
2022-08-20 13:16:14 -0700

    modelshipapi

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
+var modelShip = document.querySelector ("#star-destroyer");
+
+//Starship API call//
+var starShip= "https://swapi.dev/api/starships";
+
+var starData = "";
+
+fetch(starShip)
+.then (function(response){
+
+    return response.json();
+})
+//manipulates the dom and adds text content to HTML
+.then(function(wookie){
+    console.log(wookie);
+    starData = wookie.results[0].name.model;
+    console.log(starData);
+  
+    
+});
+
+
+
+
 const starshipBtn = document.querySelector("#submitShipBtn");
 
 const starRadioBtns = document.querySelectorAll('input[name="goalship"]');
@@ -13,20 +37,11 @@ starRadioBtns.addEventListener("click", () => {
       break;
     }
   }
+  output.innerText = selectedShip ? `you selected ${selectedShip}${modelShip}`:
 
 });
 
-
-btn.addEventListener("click", () => {
-  let selectedSize;
-  for (const radioButton of radioButtons) {
-      if (radioButton.checked) {
-          selectedSize = radioButton.value;
-          break;
-      }
-  }
-  // //show the output
-  // output.innerText = selectedShip ? `you selected ${selectedShip}`:
+  
 
 
 // var modelShip = document.querySelector ("#star-destroyer");
